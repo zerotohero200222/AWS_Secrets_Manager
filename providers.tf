8db4ffc7eca65440c30a70ec265ee4373ba27092
zerotohero200222 <aebharath17@gmail.com>
2025-07-10 15:32:33 +0530

    Update providers.tf

--- providers.tf
+++ providers.tf
@@ -5,11 +5,10 @@ terraform {
       version = "~> 5.0"
     }
     random = {
-      source = "hashicorp/random"
+      source  = "hashicorp/random"
       version = "~> 3.0"
     }
   }
-
   required_version = ">= 1.2.0"
 }
 
