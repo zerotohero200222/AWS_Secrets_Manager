13685825721b5873a85fac387a04b6d7b0d3aa01
zerotohero200222 <aebharath17@gmail.com>
2025-07-10 16:38:14 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,7 +1,8 @@
 terraform {
   backend "s3" {
-    bucket = "terraform-state-bharath-sm-why563"
+    bucket = "terraform-state-bharath-sm-why563"  # ensure all lowercase & globally unique
     key    = "secrets/dev/terraform.tfstate"
-    region = "us-east-2"
+    region = "us-east-1"
   }
 }
+
